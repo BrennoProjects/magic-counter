8101459011032eb4c12153de7acd6cfb30877a3a
brenno.gft <gft.brenno@grupofleury.com.br>
2022-07-15 14:46:30 -0300

    recovering app.tsx

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+function App() {
+  const App = styled.div`
+ 
+  `
+  return (
+    <App className="App">
+        teste
+    </App>
+  );
+}
+
+export default App
