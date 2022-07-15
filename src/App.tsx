e139738c6add88fa75cf219b9ee1c5b83b9c14f5
brennoAmaral <brenno.amaral.marques@gmail.com>
2022-07-15 20:18:43 -0300

    fix eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import styled from "styled-components";
-import { useRoutes, BrowserRouter, Router, Routes } from "react-router-dom";
+import { useRoutes } from "react-router-dom";
 import routes from "./router";
-import Home from "./screens/Home";
 
 function App() {
   const Screens = useRoutes(routes);
-  console.log(Screens, "teste");
   const App = styled.div``;
   return (
     <App className="App">
