88e3e893d36517343d71f2278ed25c612b99f38d
brennoAmaral <brenno.amaral.marques@gmail.com>
2022-07-16 13:03:23 -0300

    add background style to all pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import styled from "styled-components";
 import { useRoutes } from "react-router-dom";
 import routes from "./router";
+import palette from "./palette";
 
 function App() {
   const Screens = useRoutes(routes);
-  const App = styled.div``;
+  const App = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-color: ${palette.black};
+  `;
   return (
     <App className="App">
       {Screens}
