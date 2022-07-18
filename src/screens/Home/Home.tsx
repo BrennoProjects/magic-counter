87ca788ea54329449b9fc3de3afec7e993576104
brennoAmaral <brenno.amaral.marques@gmail.com>
2022-07-18 12:34:19 -0300

    add navigate function

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import MagicCounterLogo from "../../assets/HomePageLogo";
 import Button from "../../components/button";
+import Navigate from "../../utils/navigate";
 import * as S from "./style";
 
 const Home: FC = () => {
@@ -9,12 +10,12 @@ const Home: FC = () => {
       <MagicCounterLogo />
       <S.BoxTextButton>
         <S.H1>Magic Counter</S.H1>
-        <Button>
+        <Button onClick={()=> Navigate('/life-page')}>
           <S.TextButton>Start</S.TextButton>
         </Button>
       </S.BoxTextButton>
       <S.BoxAboutUs>
-        <Button border={false}>
+        <Button border={false} onClick={()=> Navigate('/about-us')}>
           <S.AboutUs>About us</S.AboutUs>
         </Button>
       </S.BoxAboutUs>
---
+++ src/utils/navigate.ts
@@ -0,0 +1,5 @@
+const navigate = (page: string)=> {
+  location.href=page
+}
+
+export default navigate
