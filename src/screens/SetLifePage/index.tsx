0ee9afcc111959ad8c72a46bc7854141aa921e8f
brennoAmaral <brenno.amaral.marques@gmail.com>
2022-07-20 16:01:48 -0300

    add Title and Text component on SetLifePage

--- src/screens/SetLifePage/index.tsx
+++ src/screens/SetLifePage/index.tsx
@@ -1,6 +1,8 @@
 import * as S from "./style";
 import BackArrow from "../../assets/BackArrow";
 import Button from "../../components/button";
+
+import Text from "../../components/Text";
 import Title from "../../components/Title";
 
 const SetLifePage = () => {
@@ -12,32 +14,32 @@ const SetLifePage = () => {
           onClick={() => localStorage.setItem("InitialLife", "60")}
           width="9.375rem"
         >
-          60
+          <Text>60</Text>
         </Button>
         <Button
           onClick={() => localStorage.setItem("InitialLife", "40")}
           width="9.375rem"
         >
-          40
+          <Text>40</Text>
         </Button>
       </S.WrapperButtons>
       <S.WrapperButtons>
         <Button
           onClick={() => localStorage.setItem("InitialLife", "30")}
           width="9.375rem"
-          >
-          30
+        >
+          <Text>30</Text>
         </Button>
         <Button
           onClick={() => localStorage.setItem("InitialLife", "20")}
           width="9.375rem"
         >
-          20
+          <Text>20</Text>
         </Button>
       </S.WrapperButtons>
       <S.WrapperButtons>
         <Button onClick={() => localStorage.setItem("InitialLife", "custom")}>
-          custom
+          <Text>custom</Text>
         </Button>
       </S.WrapperButtons>
     </S.WrapperSetLifePage>
