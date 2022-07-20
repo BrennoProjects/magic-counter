4a1ea8b16b42c004f1f20ce54afbaa98909f9beb
brennoAmaral <brenno.amaral.marques@gmail.com>
2022-07-20 15:15:02 -0300

    add set on localstorage initial life

--- src/screens/SetLifePage/index.tsx
+++ src/screens/SetLifePage/index.tsx
@@ -1,27 +1,40 @@
 import * as S from "./style";
 import BackArrow from "../../assets/BackArrow";
 import Button from "../../components/button";
+
 const SetLifePage = () => {
   return (
     <S.WrapperSetLifePage>
       <S.WrapperButtons>
-        <Button onClick={()=>localStorage.setItem('InitialLife', '60')} width="9.375rem">
+        <Button
+          onClick={() => localStorage.setItem("InitialLife", "60")}
+          width="9.375rem"
+        >
           60
         </Button>
-        <Button onClick={()=>localStorage.setItem('InitialLife', '40')} width="9.375rem">
+        <Button
+          onClick={() => localStorage.setItem("InitialLife", "40")}
+          width="9.375rem"
+        >
           40
         </Button>
       </S.WrapperButtons>
       <S.WrapperButtons>
-        <Button onClick={()=>localStorage.setItem('InitialLife', '30')} width="9.375rem">
+        <Button
+          onClick={() => localStorage.setItem("InitialLife", "30")}
+          width="9.375rem"
+          >
           30
         </Button>
-        <Button onClick={()=>localStorage.setItem('InitialLife', '20')} width="9.375rem">
+        <Button
+          onClick={() => localStorage.setItem("InitialLife", "20")}
+          width="9.375rem"
+        >
           20
         </Button>
       </S.WrapperButtons>
       <S.WrapperButtons>
-        <Button onClick={()=>localStorage.setItem('InitialLife', '30')}>
+        <Button onClick={() => localStorage.setItem("InitialLife", "custom")}>
           custom
         </Button>
       </S.WrapperButtons>
