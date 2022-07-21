7cb16a0ab30d6beeec37c67bab03e11324142649
brennoAmaral <brenno.amaral.marques@gmail.com>
2022-07-21 12:38:09 -0300

    rename SetlifePage to SetLife

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,6 @@
 import type { RouteObject } from "react-router-dom";
 import Home from "./screens/Home";
-import SetLifePage from "./screens/SetLifePage";
+import SetLifePage from "./screens/SetLife";
 
 const routes: RouteObject[] = [
   {path: '/', element:<Home />,},
