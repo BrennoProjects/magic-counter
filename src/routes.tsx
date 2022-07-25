cc0399d38aa17739a3cdad223a090f749b5bd6e9
brennoAmaral <brenno.amaral.marques@gmail.com>
2022-07-25 20:37:30 -0300

    add setPosition to routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,11 @@
 import type { RouteObject } from "react-router-dom";
 import Home from "./screens/Home";
 import SetLifePage from "./screens/SetLife";
+import SetPosition from "./screens/SetPosition";
 
 const routes: RouteObject[] = [
   {path: '/', element:<Home />,},
-  {path: '/set-life', element:<SetLifePage />,}
+  {path: '/set-life', element:<SetLifePage />,},
+  {path: '/set-position', element:<SetPosition />,}
 ]
 export default routes
