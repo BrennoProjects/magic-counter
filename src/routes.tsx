eeb771ead1c40984462abe7a41f7e652b6dcc1b3
brennoAmaral <brenno.amaral.marques@gmail.com>
2022-07-25 09:57:12 -0300

    add SetPlayer to routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,11 @@
 import type { RouteObject } from "react-router-dom";
 import Home from "./screens/Home";
 import SetLifePage from "./screens/SetLife";
+import SetPlayer from "./screens/SetPlayer";
 
 const routes: RouteObject[] = [
   {path: '/', element:<Home />,},
-  {path: '/set-life', element:<SetLifePage />,}
+  {path: '/set-life', element:<SetLifePage />,},
+  {path: '/set-players', element:<SetPlayer />,}
 ]
 export default routes
