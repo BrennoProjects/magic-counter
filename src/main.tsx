a32e4b08ce3340c9f85139f5bd0639739fad53f4
brennoAmaral <brenno.amaral.marques@gmail.com>
2022-07-25 13:39:59 -0300

    add PlayersNumberProvider to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -7,16 +7,19 @@ import App from "./App";
 import { dark } from "./style/theme";
 import GlobalStyles from "./style/GlobalStyles";
 import LifeProvider from "./context/LifeProvider";
+import PlayersNumberProvider from "./context/PlayersNumberProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
-      <LifeProvider>
-        <ThemeProvider theme={dark}>
-          <GlobalStyles />
-          <App />
-        </ThemeProvider>
-      </LifeProvider>
+      <PlayersNumberProvider>
+        <LifeProvider>
+          <ThemeProvider theme={dark}>
+            <GlobalStyles />
+            <App />
+          </ThemeProvider>
+        </LifeProvider>
+      </PlayersNumberProvider>
     </Router>
   </React.StrictMode>
 );
