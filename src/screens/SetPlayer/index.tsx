b5b35edf37dc33a929fb3d22c471ec478ea8602a
brennoAmaral <brenno.amaral.marques@gmail.com>
2022-07-25 13:39:11 -0300

    add setPlayers page

--- src/screens/SetPlayer/index.tsx
+++ src/screens/SetPlayer/index.tsx
@@ -1,5 +1,6 @@
+import type { FC } from "react";
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import type{ FC } from "react";
 
 import * as S from "./style";
 import BackArrow from "../../assets/BackArrow";
@@ -7,9 +8,18 @@ import Button from "../../components/button";
 import Text from "../../components/Text";
 import Title from "../../components/Title";
 import NavBar from "../../components/NavBar";
+import { PlayersNumberContext } from "../../context/PlayersNumberProvider";
 
-const SetPlayer:FC = () => {
+const SetPlayer: FC = () => {
+  const playerNumber = useContext(PlayersNumberContext);
+  let { setPlayersNumber } = playerNumber;
   const navigate = useNavigate();
+  
+  const SetAndNavigate = (value: number, path: string) => {
+    setPlayersNumber(value);
+    navigate(path)
+  };
+
   return (
     <S.WrapperSetPlayer>
       <NavBar>
@@ -25,34 +35,27 @@ const SetPlayer:FC = () => {
       <Title>Number of players</Title>
       <S.WrapperButtons>
         <Button
-          onClick={() =>  console.log('player', 1)}
+          onClick={() => SetAndNavigate(1, "/set-position")}
           width="9.375rem"
         >
           <Text>1</Text>
         </Button>
         <Button
-          onClick={() =>  console.log('player', 2)}
+          onClick={() => SetAndNavigate(2, "/set-position")}
           width="9.375rem"
         >
           <Text>2</Text>
         </Button>
       </S.WrapperButtons>
       <S.WrapperButtons>
-        <Button
-         onClick={() =>  console.log('player', 3)}
-          width="9.375rem"
-        >
+        <Button onClick={() => console.log("player", 3)} width="9.375rem">
           <Text>3</Text>
         </Button>
-        <Button
-          onClick={() =>  console.log('player', 4)}
-          width="9.375rem"
-        >
+        <Button onClick={() => console.log("player", 4)} width="9.375rem">
           <Text>4</Text>
         </Button>
       </S.WrapperButtons>
     </S.WrapperSetPlayer>
   );
 };
-
 export default SetPlayer;
