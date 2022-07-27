9dbc00a02dfc6f00a1297d6cc4bf89d8cca48b52
brennoAmaral <brenno.amaral.marques@gmail.com>
2022-07-27 13:09:45 -0300

    add position player in context

--- src/context/GameSetupContext.tsx
+++ src/context/GameSetupContext.tsx
@@ -4,8 +4,10 @@ import  { useState, createContext } from "react";
 interface GameSetupDataStructure {
   initialLife: number;
   numberPlayers: number;
+  positionPlayers: string | "default" | "twoPlayers" | "twoPlayersLeteral" | "fourPlayers" | "fourPlayersLateral";
   setInitialLife: (value: number) => void;
   setNumberPlayers: (value: number) => void;
+  setPositionPlayers: (value: string) => void;
 }
 
 interface GameSetupProps {
@@ -15,8 +17,10 @@ interface GameSetupProps {
 const initialGameSetup = {
   initialLife: 0,
   numberPlayers: 0,
+  positionPlayers: '',
   setInitialLife: () => undefined,
   setNumberPlayers: () => undefined,
+  setPositionPlayers: () => undefined,
 };
 
 export const GameSetupContext = createContext<GameSetupDataStructure>(initialGameSetup);
@@ -25,8 +29,10 @@ const GameSetup: FC<GameSetupProps> = (props) => {
   const { children } = props;
   const [initialLife, setInitialLife] = useState<number>(initialGameSetup.initialLife);
   const [numberPlayers, setNumberPlayers] = useState<number>(initialGameSetup.numberPlayers);
+  const [positionPlayers, setPositionPlayers] = useState<string>(initialGameSetup.positionPlayers);
+
   return(
-    <GameSetupContext.Provider value={{initialLife, setInitialLife, numberPlayers, setNumberPlayers}}>
+    <GameSetupContext.Provider value={{initialLife, setInitialLife, numberPlayers, setNumberPlayers, positionPlayers, setPositionPlayers}}>
       {children}
     </GameSetupContext.Provider>
   );
