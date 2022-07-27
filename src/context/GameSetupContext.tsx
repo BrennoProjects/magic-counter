ab9370d2a4a45c52591316baa1c82aae43578702
brennoAmaral <brenno.amaral.marques@gmail.com>
2022-07-27 19:43:56 -0300

    add default values to playersPositions

--- src/context/GameSetupContext.tsx
+++ src/context/GameSetupContext.tsx
@@ -1,23 +1,43 @@
-import type {  FC, ReactNode } from "react";
-import  { useState, createContext } from "react";
+import type { FC, ReactNode } from "react";
+import { useState, createContext } from "react";
+
+
+
 
 interface GameSetupDataStructure {
   initialLife: number;
   numberPlayers: number;
-  positionPlayers: string | "default" | "twoPlayers" | "twoPlayersLeteral" | "fourPlayers" | "fourPlayersLateral";
+  positionPlayers: undefined | number;
   setInitialLife: (value: number) => void;
   setNumberPlayers: (value: number) => void;
-  setPositionPlayers: (value: string) => void;
+  //setPositionPlayers: (value: "onePlayer" | "twoPlayers" | "twoPlayersLeteral" | "threePlayers" | "fourPlayers" | "fourPlayersLateral") => void;
+  setPositionPlayers: (value: number) => void;
 }
 
 interface GameSetupProps {
   children: ReactNode;
 }
 
+interface ValuesPosition {
+  default: number,
+  twoPlayers: number,
+  twoPlayersLateral: number,
+  fourPlayers: number,
+  fourPlayersLateral: number,
+}
+
+export const valuesPosition:ValuesPosition = {
+  default: 0,
+  twoPlayers: 1,
+  twoPlayersLateral: 2,
+  fourPlayers: 3,
+  fourPlayersLateral: 4,
+}
+
 const initialGameSetup = {
   initialLife: 0,
   numberPlayers: 0,
-  positionPlayers: '',
+  positionPlayers: undefined,
   setInitialLife: () => undefined,
   setNumberPlayers: () => undefined,
   setPositionPlayers: () => undefined,
@@ -26,13 +46,14 @@ const initialGameSetup = {
 export const GameSetupContext = createContext<GameSetupDataStructure>(initialGameSetup);
 
 const GameSetup: FC<GameSetupProps> = (props) => {
+  
   const { children } = props;
   const [initialLife, setInitialLife] = useState<number>(initialGameSetup.initialLife);
   const [numberPlayers, setNumberPlayers] = useState<number>(initialGameSetup.numberPlayers);
-  const [positionPlayers, setPositionPlayers] = useState<string>(initialGameSetup.positionPlayers);
+  const [positionPlayers, setPositionPlayers] = useState<undefined | number>(initialGameSetup.positionPlayers);
 
-  return(
-    <GameSetupContext.Provider value={{initialLife, setInitialLife, numberPlayers, setNumberPlayers, positionPlayers, setPositionPlayers}}>
+  return (
+    <GameSetupContext.Provider value={{ initialLife, setInitialLife, numberPlayers, setNumberPlayers, positionPlayers, setPositionPlayers }}>
       {children}
     </GameSetupContext.Provider>
   );
