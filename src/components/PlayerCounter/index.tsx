9d3ef9632617a668b5697cbeb82ee769026cc445
brennoAmaral <brenno.amaral.marques@gmail.com>
2022-07-28 07:08:51 -0300

    add WrapperPlayer

---
+++ src/components/PlayerCounter/index.tsx
@@ -0,0 +1,17 @@
+import { useCallback } from "react"
+import type { FC } from "react"
+
+import * as S from "./style"
+
+interface PlayerCounterProps{
+
+}
+
+const PlayerCounter:FC<PlayerCounterProps> =()=>{
+  return(
+    <S.WrapperPlayer>
+    </S.WrapperPlayer>
+  )
+}
+
+export default PlayerCounter
