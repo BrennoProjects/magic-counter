402a25a5987ddbaec5054a23467063f95c8bb33c
brennoAmaral <brenno.amaral.marques@gmail.com>
2022-07-28 07:12:51 -0300

    add Game page

---
+++ src/screens/Game/index.tsx
@@ -0,0 +1,17 @@
+import { useCallback } from "react"
+import type { FC } from "react"
+
+import * as S from "./style"
+
+interface GameProps{
+
+}
+
+const Game:FC<GameProps> =()=>{
+  return(
+    <S.Game>
+    </S.Game>
+  )
+}
+
+export default Game
