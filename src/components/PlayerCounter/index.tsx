ac145622d2ea075949bcc232a6de190267a686db
brennoAmaral <brenno.amaral.marques@gmail.com>
2022-07-28 12:08:36 -0300

    create basic style

--- src/components/PlayerCounter/index.tsx
+++ src/components/PlayerCounter/index.tsx
@@ -2,14 +2,26 @@ import { useCallback } from "react"
 import type { FC } from "react"
 
 import * as S from "./style"
+import HeartIcon from "../../assets/HeartIcon"
+import MinusIcon from "../../assets/MinusIcon"
+import PlusIcon from "../../assets/PlusIcon"
 
-interface PlayerCounterProps{
+
+interface PlayerCounterProps {
 
 }
 
-const PlayerCounter:FC<PlayerCounterProps> =()=>{
-  return(
+const PlayerCounter: FC<PlayerCounterProps> = () => {
+  return (
     <S.WrapperPlayer>
+      <S.WrapperText>
+        <PlusIcon width={40} height={40} />
+        <>
+        <S.TextHud fontSize="80px">40</S.TextHud>
+        <HeartIcon width={30} height={30} />
+        </>
+        <MinusIcon width={40} height={40} />
+      </S.WrapperText>
     </S.WrapperPlayer>
   )
 }
