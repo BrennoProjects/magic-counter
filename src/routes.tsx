a127d5ef166fe9091e0c990f4a12c0140788aa45
brennoAmaral <brenno.amaral.marques@gmail.com>
2022-07-28 07:09:56 -0300

    create route

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,11 +3,13 @@ import Home from "./screens/Home";
 import SetupLifePage from "./screens/SetupLife";
 import SetupPosition from "./screens/SetupPosition";
 import SetupPlayer from "./screens/SetupPlayer";
+import Game from "./screens/Game"
 
 const routes: RouteObject[] = [
   {path: '/', element:<Home />,},
   {path: '/setup-life', element:<SetupLifePage />,},
   {path: '/setup-position', element:<SetupPosition />,},
-  {path: '/setup-players', element:<SetupPlayer />,}
+  {path: '/setup-players', element:<SetupPlayer />,},
+  {path: '/Game', element:<Game />,}
 ]
 export default routes
