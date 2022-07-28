3e5c6aa21c2a6317f57d7601c1e4a33436b01575
brennoAmaral <brenno.amaral.marques@gmail.com>
2022-07-28 12:06:54 -0300

    fix svg

--- src/assets/PlusIcon/index.tsx
+++ src/assets/PlusIcon/index.tsx
@@ -8,8 +8,9 @@ interface PlusIconProps {
 const PlusIcon: FC<PlusIconProps> = (props: React.SVGProps<SVGSVGElement>) => {
   return (
     <svg width="31" height="31" viewBox="0 0 31 31" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M5 14.25H14V5.25H17V14.25L26 14.25V17.25H17V26.25H14L14 17.25H5V14.25Z" fill="white" />
+      <path d="M4 14.1071L13.8571 14.1071L13.8571 4.25H17.1429L17.1429 14.1071L27 14.1071V17.3929H17.1429V27.25H13.8571L13.8571 17.3929L4 17.3929V14.1071Z" fill="white" />
     </svg>
+
   )
 }
 
