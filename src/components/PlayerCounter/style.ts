8f1363c8d6007b0fb65f84f9ce3dcf1c3d724212
brennoAmaral <brenno.amaral.marques@gmail.com>
2022-07-28 12:08:36 -0300

    create basic style

--- src/components/PlayerCounter/style.ts
+++ src/components/PlayerCounter/style.ts
@@ -1,12 +1,34 @@
 import styled from "styled-components";
 
+interface TextHud{
+  fontSize: string,
+}
 export const WrapperPlayer = styled.div`
-  width: 95vw;
-  height: 95vh;
-  position: relative;
-  background-color: ${({theme})=> theme.purple};
+  width: 95%;
+  height: 48%;
+  display: flex;
+  
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  
+  background-color: ${({theme})=>theme.purple};
+  border-radius: 10px;
+
+`
+export const WrapperText = styled.div`
+  height: 100%;
+  width: 50%;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+`
+export const TextHud = styled.span<TextHud>`
+margin: 0;
+
+font-family: ${({theme})=>theme.font};
+font-size: ${({fontSize})=> fontSize};
+color: ${({theme})=>theme.title};
+font-weight: 600;
 `
