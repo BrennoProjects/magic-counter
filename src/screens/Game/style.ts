7f96ad4e7935131bd7dde0154a77a609b49e1b4b
brennoAmaral <brenno.amaral.marques@gmail.com>
2022-08-22 10:54:21 -0300

    fix style

--- src/screens/Game/style.ts
+++ src/screens/Game/style.ts
@@ -1,19 +1,28 @@
 import styled from "styled-components";
 
+interface RotateProps{
+  top ?: string;
+  bottom ?: string;
+}
+
 export const Game = styled.div`
   display: flex;
-  align-items: center;
   flex-direction: column;
+  align-items: center;
   justify-content: space-around;
   width: 100%;
   height: 100%;
+  overflow-x: hidden;
 `;
-export const Rotate = styled.div`
+export const Rotate = styled.div<RotateProps>`
   transform: rotate(90deg);
-  height: 95%;
-  width: 100%;
+  height: 98vw;
+  width: 52vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-around;
+  position: fixed;
+  top: ${({top}) => top};
+  bottom: ${({bottom}) => bottom}
 `;
