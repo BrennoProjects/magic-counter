881bdd1e037f7a83831310af12503901349bf749
brennoAmaral <brenno.amaral.marques@gmail.com>
2022-09-08 12:27:08 -0300

    fix lint routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,15 @@
-import type { RouteObject } from "react-router-dom";
-import Home from "./screens/Home";
-import SetupLifePage from "./screens/SetupLife";
-import SetupPosition from "./screens/SetupPosition";
-import SetupPlayer from "./screens/SetupPlayer";
-import Game from "./screens/Game"
+import type { RouteObject } from 'react-router-dom';
+import Home from './screens/Home';
+import SetupLifePage from './screens/SetupLife';
+import SetupPosition from './screens/SetupPosition';
+import SetupPlayer from './screens/SetupPlayer';
+import Game from './screens/Game';
 
 const routes: RouteObject[] = [
-  {path: '/', element:<Home />,},
-  {path: '/setup-life', element:<SetupLifePage />,},
-  {path: '/setup-position', element:<SetupPosition />,},
-  {path: '/setup-players', element:<SetupPlayer />,},
-  {path: '/Game', element:<Game />,}
-]
-export default routes
+  { path: '/', element: <Home /> },
+  { path: '/setup-life', element: <SetupLifePage /> },
+  { path: '/setup-position', element: <SetupPosition /> },
+  { path: '/setup-players', element: <SetupPlayer /> },
+  { path: '/Game', element: <Game /> }
+];
+export default routes;
