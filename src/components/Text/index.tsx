5725ea38cc31e72ed58a30ae3c62085306df14c0
brennoAmaral <brenno.amaral.marques@gmail.com>
2022-09-08 12:11:46 -0300

    fix lint components

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -1,15 +1,15 @@
-import type{ FC } from "react";
-import * as S from "./style";
+import type{ FC } from 'react';
+import * as S from './style';
 
 interface TextProps {
-  children: string;
-  margin?: string;
-  fontSize?: string;
-  bold?: boolean;
+  children: string
+  margin?: string
+  fontSize?: string
+  bold?: boolean
 }
 
 const Text: FC<TextProps> = (props) => {
-  const { children, margin = "0", fontSize = "36px", bold = true } = props;
+  const { children, margin = '0', fontSize = '36px', bold = true } = props;
   return (
     <S.FontText margin={margin} fontSize={fontSize} bold={bold}>
       {children}
