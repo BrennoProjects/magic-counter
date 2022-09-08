b11f2bfc4dfe2a634414fc8cae97bfc574dc59bb
brennoAmaral <brenno.amaral.marques@gmail.com>
2022-09-08 12:22:48 -0300

    fix lint Screens

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,29 +1,28 @@
-import type { FC } from "react";
-import { useNavigate } from "react-router-dom"
-import MagicCounterLogo from "../../assets/Logo";
-import Button from "../../components/button";
-import Text from "../../components/Text";
-import Title from "../../components/Title";
-import * as S from "./style";
-
+import type { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
+import MagicCounterLogo from '../../assets/Logo';
+import Button from '../../components/button';
+import Text from '../../components/Text';
+import Title from '../../components/Title';
+import * as S from './style';
 
 const Home: FC = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
     <S.WrapperHome>
       <MagicCounterLogo />
       <S.WrapperTextButton>
-        <Title margin={"0 0 20px 0"}>Magic Counter</Title>
-        <Button onClick={() => navigate("/setup-life")}>
+        <Title margin={'0 0 20px 0'}>Magic Counter</Title>
+        <Button onClick={() => navigate('/setup-life')}>
           <Text>Start</Text>
         </Button>
       </S.WrapperTextButton>
       <S.WrapperAboutUs>
         <Button
-          onClick={() => navigate("/about-us")}
+          onClick={() => navigate('/about-us')}
           border={false}
-          width={"auto"}
-          height={"auto"}
+          width={'auto'}
+          height={'auto'}
         >
           <S.AboutUs>About us</S.AboutUs>
         </Button>
