6215185cce5f05685a573e089974db50bd093890
brennoAmaral <brenno.amaral.marques@gmail.com>
2022-09-08 12:20:40 -0300

    fix lint GameSetupContext

--- src/context/GameSetupContext.tsx
+++ src/context/GameSetupContext.tsx
@@ -1,6 +1,5 @@
-import { ReactNode  } from "react";
-import type { FC } from "react"
-import { useState, createContext } from "react";
+import { ReactNode, useState, createContext } from 'react';
+import type { FC } from 'react';
 
 export const enum ValuesPosition {
   unset,
@@ -10,54 +9,46 @@ export const enum ValuesPosition {
   FourPlayersLateral,
 }
 
-type StartPlayer = {
-  id: string;
-  life: number;
+interface StartPlayer {
+  id: string
+  life: number
 }
 
-export type PlayerID = 'ID_PLAYER_1'|'ID_PLAYER_2'|'ID_PLAYER_3'|'ID_PLAYER_4' 
+export type PlayerID = 'ID_PLAYER_1' | 'ID_PLAYER_2' | 'ID_PLAYER_3' | 'ID_PLAYER_4';
 
 interface GameSetupDataStructure {
-  initialLife: number;
-  numberPlayers: number;
-  positionPlayers: ValuesPosition;
-  players: StartPlayer[];
-  setInitialLife: (value: number) => void;
-  setNumberPlayers: (value: number) => void;
-  setPositionPlayers: (value: ValuesPosition) => void;
-  setPlayers?: (value: StartPlayer[]) => void;
-  handleSetPlayers: (numberPlayers:number, initialLife:number)=>void,
-  handleLifePlayer: (idPlayer: PlayerID, isSum: boolean)=>void
+  initialLife: number
+  numberPlayers: number
+  positionPlayers: ValuesPosition
+  players: StartPlayer[]
+  setInitialLife: (value: number) => void
+  setNumberPlayers: (value: number) => void
+  setPositionPlayers: (value: ValuesPosition) => void
+  setPlayers?: (value: StartPlayer[]) => void
+  handleSetPlayers: (numberPlayers: number, initialLife: number) => void
+  handleLifePlayer: (idPlayer: PlayerID, isSum: boolean) => void
 }
-
-
 
 interface GameSetupProps {
-  children: ReactNode;
+  children: ReactNode
 }
-
-
-
 
 const initialGameSetup = {
   initialLife: 0,
   numberPlayers: 0,
   positionPlayers: ValuesPosition.unset,
-  players: [{id:'', life: 0}],
+  players: [{ id: '', life: 0 }],
   setInitialLife: () => undefined,
   setNumberPlayers: () => undefined,
   setPositionPlayers: () => undefined,
-  handleSetPlayers: ()=>undefined,
-  handleLifePlayer: ()=>undefined
+  handleSetPlayers: () => undefined,
+  handleLifePlayer: () => undefined
 };
 
 export const GameSetupContext =
   createContext<GameSetupDataStructure>(initialGameSetup);
 
-
-
 const GameSetup: FC<GameSetupProps> = (props) => {
-
   const { children } = props;
   const [initialLife, setInitialLife] = useState<number>(
     initialGameSetup.initialLife
@@ -71,25 +62,25 @@ const GameSetup: FC<GameSetupProps> = (props) => {
   const [players, setPlayers] = useState<StartPlayer[]>(
     initialGameSetup.players
   );
-  
-  const handleSetPlayers = (numberPlayers: number, initialLife: number) => {
-    let arrPlayersConstruct = [...Array(numberPlayers).keys()].map(
+
+  const handleSetPlayers = (numberPlayers: number, initialLife: number): void => {
+    const arrPlayersConstruct = [...Array(numberPlayers).keys()].map(
       (_value, index) => ({ id: `ID_PLAYER_${index + 1}`, life: initialLife })
-    )
-    setPlayers(arrPlayersConstruct)
-  }
-  const handleLifePlayer = (idPlayer: PlayerID, isSum: boolean) =>{
-    let newArr = [...players]
+    );
+    setPlayers(arrPlayersConstruct);
+  };
+  const handleLifePlayer = (idPlayer: PlayerID, isSum: boolean): void => {
+    const newArr = [...players];
     newArr.forEach(
       (index, key) => {
-          if(index.id===idPlayer){
-            index.life +=  isSum ? 1 : -1
-          }
-          console.log(index)
-      }    
-    )
-    setPlayers(newArr)
-  }
+        if (index.id === idPlayer) {
+          index.life += isSum ? 1 : -1;
+        }
+        console.log(index);
+      }
+    );
+    setPlayers(newArr);
+  };
 
   return (
     <GameSetupContext.Provider
